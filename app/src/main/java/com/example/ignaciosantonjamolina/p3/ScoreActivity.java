9f609ca24df5b9c7221720e651e5c04170bcbb5a
nsantonjamolina <n.santonjamolina@gmail.com>
2017-03-02 07:14:43 +0100

    Score full implementation
    Score full implementation

--- app/src/main/java/com/example/ignaciosantonjamolina/p3/ScoreActivity.java
+++ app/src/main/java/com/example/ignaciosantonjamolina/p3/ScoreActivity.java
@@ -1,9 +1,14 @@
 package com.example.ignaciosantonjamolina.p3;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.TabHost;
 import android.widget.TextView;
 
@@ -12,6 +17,33 @@ import android.widget.TextView;
  */
 
 public class ScoreActivity  extends AppCompatActivity {
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Generate the Menu object from the XML resource file
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.score_menu, menu);
+        //getMenuInflater().inflate(R.menu.example_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.borra:
+                invalidateOptionsMenu();
+                // Chapuza momentánea para borrar un dato
+                // Chapuza momentánea para borrar un dato
+                // Chapuza momentánea para borrar un dato
+                final TextView f = (TextView)findViewById(R.id.f);
+                String scoreNombre3 = f.getText().toString();
+                scoreNombre3 = "";
+                f.setText(scoreNombre3);
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -39,15 +71,35 @@ public class ScoreActivity  extends AppCompatActivity {
 
     tabs.setCurrentTab(0);
 
-//    final TextView a = (TextView)findViewById(R.id.a);
-//    String nombreScore1 = a.getText().toString();
-//        nombreScore1 += "Nacho San";
-//        a.setText(nombreScore1);
-//
-//        final TextView b = (TextView)findViewById(R.id.b);
-//        String scoreNombre1 = a.getText().toString();
-//        scoreNombre1 += "3000";
-//        b.setText(scoreNombre1);
+    final TextView a = (TextView)findViewById(R.id.a);
+    String nombreScore1 = a.getText().toString();
+        nombreScore1 = "Nacho San";
+        a.setText(nombreScore1);
+
+        final TextView b = (TextView)findViewById(R.id.b);
+        String scoreNombre1 = b.getText().toString();
+        scoreNombre1 = "3000";
+        b.setText(scoreNombre1);
+
+        final TextView c = (TextView)findViewById(R.id.c);
+        String nombreScore2 = c.getText().toString();
+        nombreScore2 = "Ignatius";
+        c.setText(nombreScore2);
+
+        final TextView d = (TextView)findViewById(R.id.d);
+        String scoreNombre2 = d.getText().toString();
+        scoreNombre2 = "6000";
+        d.setText(scoreNombre2);
+
+        final TextView e = (TextView)findViewById(R.id.e);
+        String nombreScore3 = e.getText().toString();
+        nombreScore3 = "Nacho Xan";
+        e.setText(nombreScore3);
+
+        final TextView f = (TextView)findViewById(R.id.f);
+        String scoreNombre3 = f.getText().toString();
+        scoreNombre3 = "9000";
+        f.setText(scoreNombre3);
 
 }
 }
